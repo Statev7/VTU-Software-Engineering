904b5b6a21f3bf55294f0a1d7f3223b8ee654842
Statev07 <42496790+Statev07@users.noreply.github.com>
2021-02-28 16:12:02 +0200

    Add new task

---
+++ OOP/Encapsulation - Lab/P01_SortPersonByNameAndAge/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private byte age;
+
+        public Person(string firstName, string lastName, byte age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public byte Age { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append($"{this.FirstName} {this.LastName} is {this.Age} years old.");
+            return str.ToString();
+        }
+    }
+}
---
+++ OOP/Encapsulation - Lab/P01_SortPersonByNameAndAge/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], byte.Parse(cmdArgs[2]));
+
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(a => a.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}

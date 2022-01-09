dc528a6beaa5d6a32e5ef26d62b168e3b61a5296
Statev07 <petiostatev2703@gmail.com>
2022-01-09 22:31:47 +0200

    Update

--- Java/P10_Task/src/Main.java
+++ Java/P10_Task/src/Main.java
@@ -1,16 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
-        TemperatureSensor temperatureSensor = new TemperatureSensor("temperature");
-        AtmosphericPressureSensor atmosphericPressureSensor = new AtmosphericPressureSensor("atmospheric");
-        HumiditySensor humiditySensor = new HumiditySensor("humidity");
-
+        Sensor sensor = null;
         MeteoStation meteoStation = new MeteoStation();
-        meteoStation.addSensor(temperatureSensor);
-        meteoStation.addSensor(atmosphericPressureSensor);
-        meteoStation.addSensor(humiditySensor);
+
+        BufferedReader bufferreader = new BufferedReader(new FileReader("input.txt"));
+        String line = bufferreader.readLine();
+        while (line != null){
+
+            String sensorName = line;
+
+            switch (line){
+                case "temperature":
+                    sensor = new TemperatureSensor(line);
+                    break;
+                case "atmospheric":
+                    sensor = new AtmosphericPressureSensor(line);
+                    break;
+                case "humidity":
+                    sensor = new HumiditySensor(line);
+                    break;
+            }
+
+            meteoStation.addSensor(sensor);
+            line = bufferreader.readLine();
+        }
+        bufferreader.close();
+
+        //TemperatureSensor temperatureSensor = new TemperatureSensor("temperature");
+        //AtmosphericPressureSensor atmosphericPressureSensor = new AtmosphericPressureSensor("atmospheric");
+        //HumiditySensor humiditySensor = new HumiditySensor("humidity");
 
         meteoStation.measureValues();
         Thread.sleep(1000);
